dcb2e5d088f073b24b9bc302b8c4c2939225293e
Collins Lagat <cklagat@gmail.com>
2025-06-12 15:01:00 +0300

    feat: add logic for building menus and adding vpn and bluetooth icons

--- src/tray.rs
+++ src/tray.rs
@@ -18,12 +18,22 @@ enum Icon {
 
 pub struct Tray {
     icon: Option<Icon>,
+    pub wifi_state: Option<WifiState>,
+    pub wired_state: Option<WiredState>,
+    pub bluetooth_state: Option<BluetoothState>,
+    pub vpn_state: Option<VPNState<'static>>,
+    pub airplane_mode_state: Option<AirplaneModeState>,
 }
 
 impl Tray {
     pub fn new() -> Self {
         Self {
             icon: None,
+            wifi_state: None,
+            wired_state: None,
+            bluetooth_state: None,
+            vpn_state: None,
+            airplane_mode_state: None,
         }
     }
 }
@@ -85,22 +95,32 @@ impl ksni::Tray for Tray {
         });
 
         match self.icon {
-            Some(Icon::Unknown) => vec![UNKNOWN_ICON.clone()],
-            Some(Icon::Off) => vec![OFF_ICON.clone()],
-            Some(Icon::Busy) => vec![BUSY_ICON.clone()],
-            Some(Icon::Disconnected) => vec![DISCONNECTED_ICON.clone()],
-            Some(Icon::AirplaneMode) => vec![AIRPLANE_MODE_ICON.clone()],
-            Some(Icon::Limited) => vec![LIMITED_ICON.clone()],
-            Some(Icon::Vpn) => vec![VPN_ICON.clone()],
-            Some(Icon::Ethernet) => vec![ETHERNET_ICON.clone()],
-            Some(Icon::Wifi(0..=19)) => vec![WIFI_0_ICON.clone()],
-            Some(Icon::Wifi(20..=39)) => vec![WIFI_25_ICON.clone()],
-            Some(Icon::Wifi(40..=49)) => vec![WIFI_50_ICON.clone()],
-            Some(Icon::Wifi(50..=79)) => vec![WIFI_75_ICON.clone()],
-            Some(Icon::Wifi(80..=100)) => vec![WIFI_100_ICON.clone()],
+            Some(Icon::Unknown) => icons.push(UNKNOWN_ICON.clone()),
+            Some(Icon::Off) => icons.push(OFF_ICON.clone()),
+            Some(Icon::Busy) => icons.push(BUSY_ICON.clone()),
+            Some(Icon::Disconnected) => icons.push(DISCONNECTED_ICON.clone()),
+            Some(Icon::AirplaneMode) => icons.push(AIRPLANE_MODE_ICON.clone()),
+            Some(Icon::Limited) => icons.push(LIMITED_ICON.clone()),
+            Some(Icon::Vpn) => icons.push(VPN_ICON.clone()),
+            Some(Icon::Ethernet) => icons.push(ETHERNET_ICON.clone()),
+            Some(Icon::Wifi(0..=19)) => icons.push(WIFI_0_ICON.clone()),
+            Some(Icon::Wifi(20..=39)) => icons.push(WIFI_25_ICON.clone()),
+            Some(Icon::Wifi(40..=49)) => icons.push(WIFI_50_ICON.clone()),
+            Some(Icon::Wifi(50..=79)) => icons.push(WIFI_75_ICON.clone()),
+            Some(Icon::Wifi(80..=100)) => icons.push(WIFI_100_ICON.clone()),
             Some(Icon::Wifi(_)) => unreachable!(),
-            None => vec![],
+            None => {}
+        };
+
+        if self.bluetooth_state.is_some() {
+            icons.push(BLUETOOTH_ICON.clone());
         }
+
+        if self.vpn_state.is_some() {
+            icons.push(VIRTUAL_VPN_ICON.clone());
+        }
+
+        icons
     }
 
     fn title(&self) -> String {
@@ -119,16 +139,158 @@ impl ksni::Tray for Tray {
     }
 
     fn menu(&self) -> Vec<ksni::MenuItem<Self>> {
-        use ksni::menu::SubMenu;
-        vec![
-            SubMenu {
-                label: "wireless".into(),
-                submenu: vec![],
-                ..Default::default()
+        use ksni::menu::{CheckmarkItem, RadioGroup, RadioItem, StandardItem, SubMenu};
+
+        let mut menu = vec![];
+
+        if let Some(wifi_state) = &self.wifi_state {
+            let connections = wifi_state
+                .connections
+                .iter()
+                .map(|connection| RadioItem {
+                    label: connection.into(),
+                    ..Default::default()
+                })
+                .collect::<Vec<RadioItem>>();
+
+            menu.push(
+                SubMenu {
+                    label: "Wifi".into(),
+                    submenu: vec![
+                        CheckmarkItem {
+                            label: "On".into(),
+                            checked: wifi_state.on,
+                            ..Default::default()
+                        }
+                        .into(),
+                        RadioGroup {
+                            selected: 0,
+                            options: connections,
+                            ..Default::default()
+                        }
+                        .into(),
+                    ],
+                    ..Default::default()
+                }
+                .into(),
+            );
+        }
+
+        if let Some(wired_state) = &self.wired_state {
+            menu.push(
+                SubMenu {
+                    label: "Wired".into(),
+                    submenu: vec![
+                        CheckmarkItem {
+                            label: "On".into(),
+                            checked: wired_state.on,
+                            ..Default::default()
+                        }
+                        .into(),
+                        StandardItem {
+                            label: wired_state.speed.to_string(),
+                            ..Default::default()
+                        }
+                        .into(),
+                    ],
+                    ..Default::default()
+                }
+                .into(),
+            );
+        }
+
+        if let Some(bluetooth_state) = &self.bluetooth_state {
+            let mut submenu = vec![
+                CheckmarkItem {
+                    label: "On".into(),
+                    checked: bluetooth_state.on,
+                    ..Default::default()
+                }
+                .into(),
+            ];
+
+            for device in bluetooth_state.devices.iter() {
+                submenu.push(
+                    StandardItem {
+                        label: device.clone(),
+                        ..Default::default()
+                    }
+                    .into(),
+                );
             }
-            .into(),
-        ]
+
+            menu.push(
+                SubMenu {
+                    label: "Bluetooth".into(),
+                    submenu,
+                    ..Default::default()
+                }
+                .into(),
+            );
+        }
+
+        if let Some(airplane_mode_state) = &self.airplane_mode_state {
+            menu.push(
+                CheckmarkItem {
+                    label: "Airplane Mode".into(),
+                    checked: airplane_mode_state.on,
+                    ..Default::default()
+                }
+                .into(),
+            );
+        }
+
+        if let Some(vpn_state) = &self.vpn_state {
+            menu.push(
+                SubMenu {
+                    label: "VPN".into(),
+                    submenu: vec![
+                        CheckmarkItem {
+                            label: "On".into(),
+                            checked: vpn_state.on,
+                            ..Default::default()
+                        }
+                        .into(),
+                        StandardItem {
+                            label: vpn_state.active_connection.into(),
+                            ..Default::default()
+                        }
+                        .into(),
+                    ],
+                    ..Default::default()
+                }
+                .into(),
+            );
+        }
+
+        menu
     }
+}
+
+pub struct WifiState {
+    on: bool,
+    strength: u8,
+    active_connection: String,
+    connections: Vec<String>,
+}
+
+pub struct WiredState {
+    on: bool,
+    speed: u8,
+}
+
+pub struct BluetoothState {
+    on: bool,
+    devices: Vec<String>,
+}
+
+pub struct VPNState<'a> {
+    on: bool,
+    active_connection: &'a str,
+}
+
+pub struct AirplaneModeState {
+    on: bool,
 }
 
 fn get_icon_from_image_bytes(image_bytes: &[u8]) -> ksni::Icon {
