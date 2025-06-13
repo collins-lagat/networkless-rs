b13ae149a9e06780bfac8602b8a9199a7bf3efb6
Collins Lagat <cklagat@gmail.com>
2025-06-13 20:22:44 +0300

    feat: add vpn device zbus interfaces

---
+++ src/interfaces/devices/mod.rs
@@ -0,0 +1,4 @@
+pub mod tun;
+pub mod wire_guard;
+pub mod wired;
+pub mod wireless;
--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -1,7 +1,6 @@
 pub mod access_point;
 pub mod active;
 pub mod device;
+pub mod devices;
 pub mod network_manager;
 pub mod statistics;
-pub mod wired;
-pub mod wireless;
