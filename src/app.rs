f03077b54a0fb13ff165bf1e8ebdf5f7392fcfcc
Collins Lagat <cklagat@gmail.com>
2025-06-19 22:01:16 +0300

    refactor: extract block into own function

--- src/app.rs
+++ src/app.rs
@@ -81,192 +81,8 @@ impl App {
         while let Some(event) = event_rx.recv().await {
             match event {
                 Event::Tick => {
-                    let update_tray_icon_helper = async |icon: Icon| {
-                        tray_handle
-                            .update(move |tray| {
-                                tray.set_icon(icon);
-                            })
-                            .await;
-                    };
-
-                    let state = match self.network_manager.state().await {
-                        Ok(state) => state,
-                        Err(e) => {
-                            println!("Failed to get state: {}", e);
-                            break;
-                        }
-                    };
-
-                    let is_airplane_mode = match self.network_manager.airplane_mode_enabled().await
-                    {
-                        Ok(is_airplane_mode) => is_airplane_mode,
-                        Err(e) => {
-                            println!("Failed to get airplane mode: {}", e);
-                            break;
-                        }
-                    };
-
-                    if is_airplane_mode {
-                        update_tray_icon_helper(Icon::AirplaneMode).await;
+                    if let ControlFlow::Break(_) = self.update_tray_icon(&tray_handle).await {
                         break;
-                    }
-
-                    match state {
-                        NmState::Asleep => {
-                            update_tray_icon_helper(Icon::Off).await;
-                            break;
-                        }
-                        NmState::Disconnected => {
-                            update_tray_icon_helper(Icon::Disconnected).await;
-                            break;
-                        }
-                        _ => {}
-                    };
-                    let connectivity = match self.network_manager.connectivity().await {
-                        Ok(connectivity) => connectivity,
-                        Err(e) => {
-                            println!("Failed to get connectivity: {}", e);
-                            break;
-                        }
-                    };
-                    match connectivity {
-                        NmConnectivityState::Unknown => {
-                            update_tray_icon_helper(Icon::Unknown).await;
-                            break;
-                        }
-                        NmConnectivityState::None => {
-                            update_tray_icon_helper(Icon::Disconnected).await;
-                            break;
-                        }
-                        NmConnectivityState::Portal => {
-                            todo!("handle portal");
-                        }
-                        NmConnectivityState::Loss => {
-                            update_tray_icon_helper(Icon::Limited).await;
-                            break;
-                        }
-                        _ => {}
-                    }
-                    let primary_connection = match self.network_manager.primary_connection().await {
-                        Ok(primary_connection) => primary_connection,
-                        Err(e) => {
-                            println!("Failed to get primary connection: {}", e);
-                            break;
-                        }
-                    };
-
-                    let primary_connection_id = primary_connection.id().await.unwrap();
-
-                    let devices = match primary_connection.devices().await {
-                        Ok(devices) => devices,
-                        Err(e) => {
-                            println!("Failed to get devices: {}", e);
-                            break;
-                        }
-                    };
-
-                    for device in devices {
-                        let device_type = match device.device_type().await {
-                            Ok(device_type) => device_type,
-                            Err(e) => {
-                                println!("Failed to get device type: {}", e);
-                                continue;
-                            }
-                        };
-
-                        match device_type {
-                            DeviceType::Wifi => {
-                                device
-                                    .with_connection_and_path(async |connection, path| {
-                                        let wireless_device = WirelessProxy::builder(connection)
-                                            .path(path)
-                                            .unwrap()
-                                            .build()
-                                            .await
-                                            .unwrap();
-
-                                        let active_access_point_path =
-                                            wireless_device.active_access_point().await.unwrap();
-
-                                        let active_access_point =
-                                            AccessPointProxy::builder(connection)
-                                                .path(active_access_point_path)
-                                                .unwrap()
-                                                .build()
-                                                .await
-                                                .unwrap();
-
-                                        let strength =
-                                            active_access_point.strength().await.unwrap();
-
-                                        update_tray_icon_helper(Icon::Wifi(strength)).await;
-
-                                        Ok(())
-                                    })
-                                    .await
-                                    .unwrap();
-                            }
-                            DeviceType::Ethernet => {
-                                update_tray_icon_helper(Icon::Ethernet).await;
-                            }
-                            DeviceType::TunTap => {
-                                update_tray_icon_helper(Icon::Tun).await;
-                            }
-                            DeviceType::Bluetooth => {
-                                todo!("support bluetooth in future");
-                            }
-                            DeviceType::Modem => {
-                                todo!("support modem in future");
-                            }
-                            _ => {}
-                        }
-                    }
-                    let devices = match self.network_manager.all_devices().await {
-                        Ok(devices) => devices,
-                        Err(e) => {
-                            println!("Failed to get devices: {}", e);
-                            break;
-                        }
-                    };
-
-                    let futures = devices.iter().map(async |device: &Device| {
-                        let device_type = match device.device_type().await {
-                            Ok(device_type) => device_type,
-                            Err(e) => {
-                                println!("Failed to get device type: {}", e);
-                                DeviceType::Unknown
-                            }
-                        };
-                        (matches!(device_type, DeviceType::WireGuard), device.clone())
-                    });
-                    let results = join_all(futures).await;
-                    let (has_wireguard_device, wireguard_device) = results
-                        .iter()
-                        .reduce(|acc, result| if result.0 { result } else { acc })
-                        .unwrap();
-
-                    if *has_wireguard_device {
-                        let wire_guard_connection = match wireguard_device.active_connection().await
-                        {
-                            Ok(connection) => connection,
-                            Err(e) => {
-                                println!("Failed to get wireguard connection: {}", e);
-                                break;
-                            }
-                        };
-
-                        let wire_guard_connection_id = wire_guard_connection.id().await.unwrap();
-
-                        tray_handle
-                            .update(|tray| {
-                                tray.set_vpn_state(Some(VPNState {
-                                    on: true,
-                                    active_connection: wire_guard_connection_id.clone(),
-                                }));
-                            })
-                            .await;
-                    } else {
-                        tray_handle.update(|tray| tray.set_vpn_state(None)).await;
                     }
 
                     continue;
@@ -274,6 +90,185 @@ impl App {
                 Event::Shutdown => break,
             }
         }
+    }
+
+    async fn update_tray_icon(&self, tray_handle: &Handle<Tray>) -> ControlFlow<()> {
+        let update_tray_icon_helper = async |icon: Icon| {
+            tray_handle
+                .update(move |tray| {
+                    tray.set_icon(icon);
+                })
+                .await;
+        };
+        let state = match self.network_manager.state().await {
+            Ok(state) => state,
+            Err(e) => {
+                println!("Failed to get state: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        let is_airplane_mode = match self.network_manager.airplane_mode_enabled().await {
+            Ok(is_airplane_mode) => is_airplane_mode,
+            Err(e) => {
+                println!("Failed to get airplane mode: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        if is_airplane_mode {
+            update_tray_icon_helper(Icon::AirplaneMode).await;
+            return ControlFlow::Break(());
+        }
+        match state {
+            NmState::Asleep => {
+                update_tray_icon_helper(Icon::Off).await;
+                return ControlFlow::Break(());
+            }
+            NmState::Disconnected => {
+                update_tray_icon_helper(Icon::Disconnected).await;
+                return ControlFlow::Break(());
+            }
+            _ => {}
+        };
+        let connectivity = match self.network_manager.connectivity().await {
+            Ok(connectivity) => connectivity,
+            Err(e) => {
+                println!("Failed to get connectivity: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        match connectivity {
+            NmConnectivityState::Unknown => {
+                update_tray_icon_helper(Icon::Unknown).await;
+                return ControlFlow::Break(());
+            }
+            NmConnectivityState::None => {
+                update_tray_icon_helper(Icon::Disconnected).await;
+                return ControlFlow::Break(());
+            }
+            NmConnectivityState::Portal => {
+                todo!("handle portal");
+            }
+            NmConnectivityState::Loss => {
+                update_tray_icon_helper(Icon::Limited).await;
+                return ControlFlow::Break(());
+            }
+            _ => {}
+        }
+        let primary_connection = match self.network_manager.primary_connection().await {
+            Ok(primary_connection) => primary_connection,
+            Err(e) => {
+                println!("Failed to get primary connection: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        let primary_connection_id = primary_connection.id().await.unwrap();
+        let devices = match primary_connection.devices().await {
+            Ok(devices) => devices,
+            Err(e) => {
+                println!("Failed to get devices: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        for device in devices {
+            let device_type = match device.device_type().await {
+                Ok(device_type) => device_type,
+                Err(e) => {
+                    println!("Failed to get device type: {}", e);
+                    continue;
+                }
+            };
+
+            match device_type {
+                DeviceType::Wifi => {
+                    device
+                        .with_connection_and_path(async |connection, path| {
+                            let wireless_device = WirelessProxy::builder(connection)
+                                .path(path)
+                                .unwrap()
+                                .build()
+                                .await
+                                .unwrap();
+
+                            let active_access_point_path =
+                                wireless_device.active_access_point().await.unwrap();
+
+                            let active_access_point = AccessPointProxy::builder(connection)
+                                .path(active_access_point_path)
+                                .unwrap()
+                                .build()
+                                .await
+                                .unwrap();
+
+                            let strength = active_access_point.strength().await.unwrap();
+
+                            update_tray_icon_helper(Icon::Wifi(strength)).await;
+
+                            Ok(())
+                        })
+                        .await
+                        .unwrap();
+                }
+                DeviceType::Ethernet => {
+                    update_tray_icon_helper(Icon::Ethernet).await;
+                }
+                DeviceType::TunTap => {
+                    update_tray_icon_helper(Icon::Tun).await;
+                }
+                DeviceType::Bluetooth => {
+                    todo!("support bluetooth in future");
+                }
+                DeviceType::Modem => {
+                    todo!("support modem in future");
+                }
+                _ => {}
+            }
+        }
+        let devices = match self.network_manager.all_devices().await {
+            Ok(devices) => devices,
+            Err(e) => {
+                println!("Failed to get devices: {}", e);
+                return ControlFlow::Break(());
+            }
+        };
+        let futures = devices.iter().map(async |device: &Device| {
+            let device_type = match device.device_type().await {
+                Ok(device_type) => device_type,
+                Err(e) => {
+                    println!("Failed to get device type: {}", e);
+                    DeviceType::Unknown
+                }
+            };
+            (matches!(device_type, DeviceType::WireGuard), device.clone())
+        });
+        let results = join_all(futures).await;
+        let (has_wireguard_device, wireguard_device) = results
+            .iter()
+            .reduce(|acc, result| if result.0 { result } else { acc })
+            .unwrap();
+
+        if *has_wireguard_device {
+            let wire_guard_connection = match wireguard_device.active_connection().await {
+                Ok(connection) => connection,
+                Err(e) => {
+                    println!("Failed to get wireguard connection: {}", e);
+                    return ControlFlow::Break(());
+                }
+            };
+
+            let wire_guard_connection_id = wire_guard_connection.id().await.unwrap();
+
+            tray_handle
+                .update(|tray| {
+                    tray.set_vpn_state(Some(VPNState {
+                        on: true,
+                        active_connection: wire_guard_connection_id.clone(),
+                    }));
+                })
+                .await;
+        } else {
+            tray_handle.update(|tray| tray.set_vpn_state(None)).await;
+        }
+        ControlFlow::Continue(())
     }
 }
 
