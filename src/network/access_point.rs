bcf9d27ae5511d4a81713c6bebc5b84ad41076f1
Collins Lagat <cklagat@gmail.com>
2025-06-22 19:52:55 +0300

    feat: extract wifi id/name from ssid

--- src/network/access_point.rs
+++ src/network/access_point.rs
@@ -3,12 +3,26 @@ use zbus::Result;
 use crate::interfaces::access_point::AccessPointProxy;
 
 pub struct AccessPoint {
+    id: Option<String>,
     access_point: AccessPointProxy<'static>,
 }
 
 impl AccessPoint {
     pub fn new(access_point: AccessPointProxy<'static>) -> Self {
-        Self { access_point }
+        Self {
+            id: None,
+            access_point,
+        }
+    }
+
+    pub async fn id(&mut self) -> Result<&str> {
+        if self.id.is_some() {
+            return Ok(self.id.as_ref().unwrap());
+        }
+        let ssid = self.access_point.ssid().await?;
+        let id = String::from_utf8_lossy(&ssid);
+        self.id = Some(id.to_string());
+        Ok(self.id.as_ref().unwrap())
     }
 
     pub async fn strength(&self) -> Result<u8> {
