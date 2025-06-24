5fff17f56b0112c8adc8c8c3cc8dc6defd7b7f51
Collins Lagat <cklagat@gmail.com>
2025-06-24 13:12:45 +0300

    feat: provide convinient methods to get saved connection's id and uuid

--- src/network/settings.rs
+++ src/network/settings.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+
 use crate::interfaces::settings::connection::ConnectionProxy;
 
 pub struct ConnectionSetting {
@@ -7,5 +9,27 @@ pub struct ConnectionSetting {
 impl ConnectionSetting {
     pub fn new(settings: ConnectionProxy<'static>) -> Self {
         Self { settings }
+    }
+
+    pub async fn id(&self) -> Result<String> {
+        let settings = self.settings.get_settings().await?;
+        if let Some(connection) = settings.get("connection") {
+            let id = connection.get("id").unwrap().to_owned();
+            let id = String::try_from(id).unwrap();
+            Ok(id)
+        } else {
+            anyhow::bail!("No id found")
+        }
+    }
+
+    pub async fn uuid(&self) -> Result<String> {
+        let settings = self.settings.get_settings().await?;
+        if let Some(connection) = settings.get("connection") {
+            let uuid = connection.get("uuid").unwrap().to_owned();
+            let uuid = String::try_from(uuid).unwrap();
+            Ok(uuid)
+        } else {
+            anyhow::bail!("No uuid found")
+        }
     }
 }
